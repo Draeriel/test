00076eab89eaedaf011c966905d406606ff71c39
draeriel <danieljuanbarbosa@gmail.com>
2018-03-12 19:57:19 +0100

    wiiii!

--- main.js
+++ main.js
@@ -1,49 +1,67 @@
-window.onload=function (){
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          myFunction(this);
-      }
-  };
-  xhttp.open("GET", "main.xml", true);
-  xhttp.send();
-  var inicial = 0;
-  var resp1 = 0;
-  var resp2 = 1;
+var inicial = 0;
+var resp1 = 0;
+var resp2 = 1;
 
-  function myFunction(xml) {
-      var xmlDoc = xml.responseXML;
-      
+window.onload=function() {
 
-      document.getElementById("pregunta").innerHTML =
-      xmlDoc.getElementsByTagName("question")[inicial].childNodes[0].nodeValue;
-      
-      document.getElementById("imagen").src =
-      xmlDoc.getElementsByTagName("imagen")[inicial].childNodes[0].nodeValue;
-      
-      document.getElementById("respuesta1").innerHTML =
-      xmlDoc.getElementsByTagName("answer")[resp1].childNodes[0].nodeValue;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+    };
 
-      document.getElementById("respuesta2").innerHTML =
-      xmlDoc.getElementsByTagName("answer")[resp2].childNodes[0].nodeValue;   
-  }
+    xhttp.open("GET", "main.xml", true);
+    xhttp.send();
 
-  document.getElementById("next").onclick = function() {next()};
-  function next() {
-      inicial = inicial + 1;
-      resp1 = resp1 + 2;
-      resp2 = resp2 + 2;
-      
-      document.getElementById("pregunta").innerHTML =
-      xml.responseXML.getElementsByTagName("question")[inicial].childNodes[0].nodeValue;
-      
-      document.getElementById("imagen").src =
-      xml.responseXML.getElementsByTagName("imagen")[inicial].childNodes[0].nodeValue;
-      
-      document.getElementById("respuesta1").innerHTML =
-      xml.responseXML.getElementsByTagName("answer")[resp1].childNodes[0].nodeValue;
+    function myFunction(xml) {
+        var xmlDoc = xml.responseXML;
+        
+        document.getElementById("pregunta").innerHTML =
+        xmlDoc.getElementsByTagName("question")[inicial].childNodes[0].nodeValue;
+        
+        document.getElementById("imagen").src =
+        xmlDoc.getElementsByTagName("imagen")[inicial].childNodes[0].nodeValue;
+        
+        document.getElementById("respuesta1").innerHTML =
+        xmlDoc.getElementsByTagName("answer")[resp1].childNodes[0].nodeValue;
 
-      document.getElementById("respuesta2").innerHTML =
-      xml.responseXML.getElementsByTagName("answer")[resp2].childNodes[0].nodeValue; 
-  }
+        document.getElementById("respuesta2").innerHTML =
+        xmlDoc.getElementsByTagName("answer")[resp2].childNodes[0].nodeValue;   
     }
+}
+
+function next() {
+    inicial = inicial + 1;
+    resp1 = resp1 + 2;
+    resp2 = resp2 + 2;
+    hi()
+}
+
+function hi() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+    };
+
+    xhttp.open("GET", "main.xml", true);
+    xhttp.send();
+
+    function myFunction(xml) {
+        var xmlDoc = xml.responseXML;
+        
+        document.getElementById("pregunta").innerHTML =
+        xmlDoc.getElementsByTagName("question")[inicial].childNodes[0].nodeValue;
+        
+        document.getElementById("imagen").src =
+        xmlDoc.getElementsByTagName("imagen")[inicial].childNodes[0].nodeValue;
+        
+        document.getElementById("respuesta1").innerHTML =
+        xmlDoc.getElementsByTagName("answer")[resp1].childNodes[0].nodeValue;
+
+        document.getElementById("respuesta2").innerHTML =
+        xmlDoc.getElementsByTagName("answer")[resp2].childNodes[0].nodeValue;   
+    }
+}
